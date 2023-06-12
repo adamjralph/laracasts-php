d45c6671cf55f84f31cd22bb5d25c0895442b2ad
Adam Ralph <adamralph@posteo.net>
2023-06-12 20:25:24 +1000

    Initialize!

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHP Demo</title>
+  <link rel="stylesheet" href="./styles.css">
+</head>
+<body>
+
+  <h1>
+    You have read "Neuromancer"
+  </h1>
+  
+</body>
+</html>
